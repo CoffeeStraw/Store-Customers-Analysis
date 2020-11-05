6527b6fde988235e1f2f154356cddcf04e649f89
CoffeeStraw <clarantonio98@gmail.com>
2020-11-05 15:30:53 +0100

    WIP Rework of the outliers removal, added several prints and fixes to semantical analysis

--- project/main.py
+++ project/main.py
@@ -70,70 +70,99 @@ def semantical_analysis(df: pd.DataFrame):
     """
     # PRE: Check if basket starting with 'C' all have quantity less than 0
     # Results: only basket starting with 'C' have quantity less than 0
-    # tmp = df[ (df["BasketID"].str.contains('C')) & (df["Qta"] > 0) ]
-    # tmp = df[ df["Qta"] < 0 ]
+    tmp = df[ (df["BasketID"].str.contains('C')) & (df["Qta"] > 0) ]
+    print("N. BasketID STARTING WITH 'C' AND Qta > 0:", len(tmp))
+    
+    tmp = df[ (df["Qta"] < 0) & ~(df["BasketID"].str.contains('C')) ]
+    tmp.dropna(subset=['CustomerID'], inplace=True)
+    print("N. BasketID WITH Qta < 0:", len(tmp))
 
     # Check if we have the same product inside the same basket
     # Result: two cases, same price, different price
+    done = False
     def check_duplicated_prods(x):
-        if len(x) > 1:
-            if x["Sale"].nunique() == 1:
-                print(x)
+        nonlocal done
+        if len(x) > 1 and x["Sale"].nunique() == 1 and not done:
+            print("BASKET WITH INCONSISTENT Qta:\n", x)
+            done = True
     df.groupby(['BasketID','BasketDate','ProdID']).apply(check_duplicated_prods)
 
     # Check if with same BasketID we have different datetimes
     # Results: change BasketDate to PurchaseDate
     tmp = df.groupby(["BasketID"]).nunique()["BasketDate"].eq(1)
     tmp = tmp[tmp == False]
+    print("INCONSISTENT BasketDates:", len(tmp))
 
-    # Check if two customers happend to have the same BasketID
+    # Check if two customers happen to have the same BasketID
     # Result: after removing duplicates no other wrong value found
     tmp = df.groupby(["BasketID", "CustomerID"]).ngroups
+    print("N. BasketID-CustomerID COUPLES:", tmp)
 
-    tmp = df.groupby(["BasketID"]).ngroups
+    tmp = df["BasketID"].nunique()
+    print("N. BasketID:", tmp)
 
-    tmp = df.groupby(["BasketID"]).nunique()["CustomerID"].eq(1)
+    tmp = df.dropna(subset=['CustomerID'])
+    tmp = tmp.groupby(["BasketID"]).nunique()["CustomerID"].eq(1)
     tmp = tmp[tmp == False].index
+    print("INCONSITENT BasketID-CustomerID (after NaN removal):", len(tmp))
 
     # Check if discount are always alone in the basket
     # Result: Almost always, only one time we have it together with Manual
     tmp = df[
-        df["BasketID"].isin(df[df['ProdID'] == "D"]["BasketID"])
-    ]
-    tmp = tmp[tmp["ProdID"] != "D"]["ProdDescr"]
+        df["BasketID"].isin(
+            df[df['ProdID'] == "D"]["BasketID"]
+    )]
+    tmp = tmp[tmp["ProdID"] != "D"]
+    print("PRODUCTS IN THE SAME BASKET WITH DISCOUNT:\n", tmp)
     
     # Check if baskets only are numerical with an optional starting 'C' character
     # Result: We found baskets starting with 'A', which however will be removed since they have sales less than 0
     tmp = df[~df['BasketID'].str.contains('C')][df['BasketID'].str.contains('[A-Za-z]')]["BasketID"].unique()
+    print("STRANGE BASKETS:\n", tmp)
 
     # Check for strange ProductID (nor alphanumerical code only)
     # Result: A lot of products contains characters, we get to know about discounts and bank charges
     tmp = df[df['ProdID'].str.contains('[A-Za-z]')]["ProdID"].unique()
+    print("STRANGE ProductID:\n", tmp)
 
     # Check for non-uppercase descriptions
-    # Result: we get to know about descriptions being inconsistent
-    tmp = df[df['ProdDescr'].str.contains('[a-z]')]["ProdDescr"].unique()
+    # Result: we get to know about descriptions being inconsistent and some strange descriptions
+    tmp = df['ProdDescr'].isna().sum()
+    print("N. NaN ProdDescr:", tmp)
+
+    tmp = df.dropna(subset=['ProdDescr'])
+    tmp = tmp[tmp['ProdDescr'].str.contains('[a-z]')]["ProdDescr"].unique()
+    print("INCONSISTENT ProdDescr:\n", tmp)
 
     # Check list of countries
-    # Result: (Get to know about hidden null-values: 'Unknown')
+    # Result: (Get to know about hidden null-values: 'Unspecified')
     tmp = list(sorted(list(df["CustomerCountry"].unique())))
+    print("COUNTRIES:", tmp)
 
     # Check for strange qta values
     # Result: Get to know about negative values and outliers
-    tmp = df['Qta'].unique()
+    tmp = df['Qta'].describe()
+    print("Qta Descr:", tmp)
 
     # CustomerCountry seems like the country where the user registered... is that true?
-    # Result: no, since some IDs have different countries. Change name to 'PurchaseCountry'
+    # Result: no, since some IDs have different countries. Some customers may have changed their nationality.
+    # We will take this into account when we will create the customer profilation dataset.
     tmp = df.groupby(["CustomerID"]).nunique()["CustomerCountry"].eq(1)
+    tmp = list(tmp[tmp == False].index)
+    print("INCONSISTENT CustomerCountry:", tmp)
 
     # Do all ProdID have one ProdDescr?
     # Result: No, some descriptions are more verbose, we will take those
     tmp = df.groupby(["ProdID"]).nunique()["ProdDescr"].eq(1)
     tmp = tmp[tmp == False].index
-    tmp = df[df["ProdID"].isin(tmp)].groupby("ProdID").aggregate({'ProdDescr': 'max'})
+    print("N. INCONSISTENT ProdDescr:", len(tmp))
 
     # Do we have sales with more than 3 digit places?
-    tmp = df["Sale"].astype(str).str.contains(r"\.\d{3}").value_counts()
+    # Result: Yes, we will round them
+    tmp = df["Sale"].astype(str).str.contains(r",\d{3,}")
+    tmp = tmp[tmp == True].index
+    tmp = df.loc[tmp]
+    print("INCONSISTENT Sale:\n", tmp)
 
 
 def fix_dataset(df: pd.DataFrame):
@@ -154,6 +183,7 @@ def fix_dataset(df: pd.DataFrame):
     df["ProdID"] = df["ProdID"].str.upper()
 
     # Remove purchases with prices less than or equal to zero, together with some outliers that costs less than 0.01
+    # We remove them since they're few
     df = df[df["Sale"] >= 0.01]
 
     # Remove C from basketIDs, since it is pointless (we already have negative quantities to identify those)
@@ -173,39 +203,31 @@ def fix_dataset(df: pd.DataFrame):
 
     df[["ProdID", "ProdDescr"]] = df[["ProdID", "ProdDescr"]].apply(uniform_descr, axis=1)
 
-    # Put multiple products in the same basket as a single product (only ones with same sale)
-    df = df.groupby(['BasketID','ProdID', 'Sale']).agg({
+    # Put multiple products in the same basket as a single product
+    df = df.groupby(['BasketID','ProdID']).agg({
         'BasketDate': 'min',
         'Qta': 'sum',
+        'Sale': 'mean',
         'CustomerID': 'min',
         'CustomerCountry': 'min',
         'ProdDescr': 'min'
     }).reset_index()
-    
-    # Remove products in the same basket, but having different sales
-    def fun(x):
-        if len(x) > 1:
-            if x["Qta"].iloc[0] > 0 and x["ProdID"].iloc[0] != "M":
-                return x
-
-    tmp = df.groupby(['BasketID','ProdID']).apply(fun).drop_duplicates().dropna()[["BasketID", "ProdID"]]
-    df = df.drop(tmp.index)
 
     # === REMOVE THE OUTLIERS ===
     plt.rcParams['figure.figsize'] = 10, 10
     def iqr_non_outliers(s: pd.Series):
-        """
-        Returns a true-list of the outliers in a column of the DataFrame,
-        based on the quantiles
-        """
+        # Returns a true-list of the outliers in a column of the DataFrame,
+        # based on the quantiles
+        
         Q1 = s.quantile(0.25)
         Q3 = s.quantile(0.75)
+
         IQR = Q3 - Q1
-        trueList = ~((s < (Q1 - 1.5 * IQR)) |(s > (Q3 + 1.5 * IQR)))
+        trueList = ~((s < (Q1 - 1.5 * IQR)) | (s > (Q3 + 1.5 * IQR)))
         return trueList
 
-    # Remove outliers from Sale based on Z-Score
-    df_sale = df["Sale"]
+    # Remove outliers from Sale over 0.9999 percentile (approximately 1000)
+    df_sale = df['Sale']
 
     df_sale.plot.box()
     plt.tight_layout()
@@ -217,9 +239,14 @@ def fix_dataset(df: pd.DataFrame):
     plt.savefig("../report/imgs/Outliers_Sale_Distribution")
     plt.close()
 
-    df = df[abs(stats.zscore(df_sale)) < 3]
+    df[df_sale > 1000]["Sale"].hist(bins=100)
+    plt.tight_layout()
+    plt.savefig("../report/imgs/Outliers_Sale_Clipped")
+    plt.close()
 
-    # Remvoe outliers from Qta with IQR method
+    df = df[df_sale <= 1000]
+
+    # Remove outliers from Qta based on a threshold of 3500
     df_qta = df["Qta"]
 
     df_qta.plot.box()
@@ -232,9 +259,25 @@ def fix_dataset(df: pd.DataFrame):
     plt.savefig("../report/imgs/Outliers_Qta_Distribution")
     plt.close()
 
-    df = df[iqr_non_outliers(df_qta)]
+    # Let's check if iqr is effective
+    # Result: no, since we think that most of the customers are wholesalers and it would drop too many entries
+    # We will use a threshold which we defined by trying and plotting several box plots
+    qta_iqr_outliers = df_qta[~iqr_non_outliers(df_qta)]
+    print("QTA - IQR RESULTS:\n", qta_iqr_outliers.describe())
+    print("MIN Qta Positives:", qta_iqr_outliers[qta_iqr_outliers > 0].min())
+    print("MAX Qta Negatives:", qta_iqr_outliers[qta_iqr_outliers < 0].max())
+    
+    # Let's check if the outliers comes from different users
+    # TODO: fai vedere come abbiamo scelto 3500 mostrando i diversi boxplot
+    outliers = df.loc[df_qta[abs(df_qta) >= 3500].index]
+    print("QTA OUTLIERS (with threshold of 3500):")
+    print(outliers["Qta"].describe())
+    print(outliers["CustomerID"].nunique())
 
-    # Remove outliers from BasketID based on zscore
+    # Values come from different users, we cannot just drop the customers, must instead drop single tuples
+    df["Qta"] = df_qta[abs(df_qta) < 3500]
+
+    # Remove outliers from BasketID based on IQR
     df_articles_per_basket = df[["BasketID", "Qta"]].groupby("BasketID").agg('sum')["Qta"]
 
     df_articles_per_basket.plot.box()
@@ -247,10 +290,20 @@ def fix_dataset(df: pd.DataFrame):
     plt.savefig("../report/imgs/Outliers_BasketID_Distribution")
     plt.close()
 
-    bid_good = df_articles_per_basket[abs(stats.zscore(df_articles_per_basket)) < 3].index
-    df = df[df["BasketID"].isin(bid_good)]
+    # Check if IQR 
+    basketid_iqr_outliers = df_articles_per_basket[~iqr_non_outliers(df_articles_per_basket)]
+    print("BASKETID - IQR RESULTS:\n", basketid_iqr_outliers.describe())
+    print("MIN BASKETID-QTA Postives:", basketid_iqr_outliers[basketid_iqr_outliers > 0].min())
+    print("MAX BASKETID-QTA Negatives:", basketid_iqr_outliers[basketid_iqr_outliers < 0].max())
 
-    # Remove outliers from CustomerID based on zscore
+    # Remove outliers based on a threshold
+    # TODO: Fai vedere i boxplot
+    non_outliers = df_articles_per_basket[abs(df_articles_per_basket) < 5000]
+    df = df[df["BasketID"].isin(non_outliers.index)]
+
+    df_articles_per_basket = df[["BasketID", "Qta"]].groupby("BasketID").agg('sum')["Qta"]
+
+    # Remove outliers from CustomerID based on IQR
     df_totsale_per_user = pd.Series([round( sum( g[1]["Sale"]*g[1]["Qta"] ), 2) for g in df.groupby('CustomerID')], index=[g[0] for g in df.groupby('CustomerID')])
     
     df_totsale_per_user.plot.box()
@@ -263,8 +316,14 @@ def fix_dataset(df: pd.DataFrame):
     plt.savefig("../report/imgs/Outliers_CustomerID_Distribution")
     plt.close()
 
-    cid_good = df_totsale_per_user[abs(stats.zscore(df_totsale_per_user)) < 3].index
-    df = df[df["CustomerID"].isin(cid_good)]
+    # Try with IQR
+    customerid_iqr_outliers = df_totsale_per_user[~iqr_non_outliers(df_totsale_per_user)]
+    print("CUSTOMERID - IQR RESULTS:\n", customerid_iqr_outliers.describe())
+
+    # Remove based on a threshold
+    # TODO: Fai box plots (forse basta quello generato)
+    non_outliers = df_totsale_per_user[abs(df_totsale_per_user) < 50000]
+    df = df[df["CustomerID"].isin(non_outliers.index)]
 
     # Drop rows corresponding to returns without relative purchase (inconsistent data)
     invalid_indexes = []
@@ -274,16 +333,17 @@ def fix_dataset(df: pd.DataFrame):
         for i, qta in enumerate(x['Qta']):
             if (s := s + qta) < 0:
                 invalid_indexes.append(x.iloc[i].name)
-
+                s = 0
+    
     df[ ~df["ProdID"].isin(['M', 'D', 'BANK CHARGES']) ].groupby(['CustomerID', 'ProdID']).apply(get_invalid_indexes)
-    print("N. of dropped inconsistent returns:", len(invalid_indexes))
     df.drop(invalid_indexes, inplace=True)
+    print("N. of dropped inconsistent returns:", len(invalid_indexes))
 
     # Rename columns with names that could mislead
-    df.rename(columns={'CustomerCountry': 'PurchaseCountry', 'BasketDate': 'PurchaseDate'}, inplace=True)
+    df.rename(columns={'BasketDate': 'PurchaseDate'}, inplace=True)
 
     # Swap columns
-    df = df[["BasketID", "ProdID", "Sale", "Qta", "ProdDescr", "PurchaseDate", "PurchaseCountry", "CustomerID"]]
+    df = df[["BasketID", "ProdID", "ProdDescr", "Sale", "Qta", "PurchaseDate", "CustomerID","CustomerCountry"]]
 
     # Sort by date the dataset and reset indexes
     df.sort_values("PurchaseDate", inplace=True)
@@ -447,7 +507,7 @@ def customer_profilation(df: pd.DataFrame):
     preferred_item = lambda g: g.groupby('ProdID').agg({'Qta':'sum'}).idxmax()[0]
     # Main country
     main_country = lambda g: g[['BasketID','PurchaseCountry']].groupby('PurchaseCountry').nunique().idxmax()[0] 
-    #Number of baskets
+    # Number of baskets
     n_baskets = lambda g:  g['BasketID'].nunique()
 
     groups = df[df["Qta"]>0].groupby("CustomerID")
@@ -480,6 +540,8 @@ def customer_profilation(df: pd.DataFrame):
         [ round(-groups.loc[i]['Qta']/cdf.loc[i]['l']*100, 2) if i in groups.index else 0 for i in cdf.index ],
         index=cdf.index
     )
+    cdf['SaleRate'] = cdf['TotSale']/cdf['l'] 
+
     print(cdf)
     cdf.to_csv("customer_profilation.csv")
 
@@ -517,9 +579,9 @@ def customer_statistics(cdf: pd.DataFrame):
 
     # lu vs E
     cdf['log(lu)']=np.log(cdf['lu'])
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12,3))
-    cdf.plot.scatter('lu','E',c='lmax',colormap='viridis',ax=axes[0])
-    cdf.plot.scatter('log(lu)','E',c='lmax',colormap='viridis' ,ax = axes[1]  )
+    _, axes = plt.subplots(nrows=1, ncols=2, figsize=(12,3))
+    cdf.plot.scatter('lu','E',c='l',colormap='viridis',ax=axes[0])
+    cdf.plot.scatter('log(lu)','E',c='l',colormap='viridis' ,ax = axes[1]  )
     plt.tight_layout()
     plt.savefig("../report/imgs/cdf_lu_vs_E")
     plt.close()
@@ -560,8 +622,9 @@ if __name__ == "__main__":
 
     # Prints data's samples and informations,
     # including the number of not null values for each columns
-    print(df.info(), "\n")
-    print(df.head())
+    # print(df.info(), "\n")
+    # print(df.head())
+    # semantical_analysis(df)
 
     fix_dataset(df)
     quit()
@@ -573,11 +636,117 @@ if __name__ == "__main__":
     # distribution_and_statistics(df)
     
     # === CUSTOMER PROFILATION ===
-    customer_profilation(df)
-    quit()
+    # customer_profilation(df)
     cdf = pd.read_csv('customer_profilation.csv', index_col=0)
+    """
     customer_statistics(cdf)
+    """
 
+    from sklearn.preprocessing import StandardScaler, MinMaxScaler
+    from sklearn.cluster import KMeans
+    from sklearn.metrics import silhouette_score
+    from sklearn import metrics
+    from itertools import combinations
+
+    # Reimpostazione dataframe
+    # cdf = cdf[cdf['MainCountry'] == 'United Kingdom']
+    attr_cluster = ['E','SaleRate','NBaskets']
+    cdf = cdf[attr_cluster]
+    # cdf['MainCountry'] = pd.factorize(cdf['MainCountry'])[0]
+    # cdf['PrefItem'] = pd.factorize(cdf['PrefItem'])[0]
+
+    def iqr_non_outliers(s: pd.Series):
+        # Returns a true-list of the outliers in a column of the DataFrame,
+        # based on the quantiles
+        Q1 = s.quantile(0.25)
+        Q3 = s.quantile(0.75)
+        IQR = Q3 - Q1
+        trueList = ~((s < (Q1 - 1.5 * IQR)) |(s > (Q3 + 1.5 * IQR)))
+        return trueList
+
+    cdf = cdf[iqr_non_outliers(cdf['SaleRate'])]
+
+    # normalizzazione (prova a denormalizzare)
+    scaler = MinMaxScaler() # Minmax?
+    X = scaler.fit_transform(cdf.values)
+
+    #selezionare miglior valore di k
+    """
+    sse_list = list()
+    max_k = 30
+    for k in range(2, max_k + 1):
+        kmeans = KMeans(n_clusters=k, n_init=10, max_iter=100)
+        kmeans.fit(X)
+        
+        sse = kmeans.inertia_
+        sse_list.append(sse)
+
+    plt.plot(range(2, len(sse_list) + 2), sse_list, marker='o')
+    plt.ylabel('SSE', fontsize=22)
+    plt.xlabel('K', fontsize=22)
+    plt.show()
+    """
+
+    # clusterizzazione
+    kmeans = KMeans(n_clusters=4, n_init=10, max_iter=1000)
+    kmeans.fit(X)
+    centers = kmeans.cluster_centers_
+
+    # grafico 2D
+    """
+    plt.scatter(cdf['E'], cdf['SaleRate'], c=kmeans.labels_, s=20)
+    plt.scatter(centers[:, 0], centers[:, 1], s=200, marker='*', c='k')
+    plt.show()
+    quit()
+    """
+
+    combos = list(combinations(attr_cluster, 3))
+
+    for c in combos:
+        c1, c2, c3 = c
+
+        # Grafico 3D
+        fig = plt.figure(figsize = (10, 7))
+        ax = plt.axes(projection ="3d")
+
+        ax.scatter3D(cdf[c1], cdf[c2], cdf[c3], c=kmeans.labels_, s=20)
+        ax.scatter3D(centers[:, 0], centers[:, 1], centers[:, 2], s=200, marker='*', c='k')
+        ax.set_xlabel(c1)
+        ax.set_ylabel(c2)
+        ax.set_zlabel(c3)
+        plt.show()
+    
+
+    """
+    from math import pi
+ 
+    # number of variable
+    N = len(cdf.columns)
+    # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
+    for i in range(0, len(centers)):
+        angles = [n / float(N) * 2 * pi for n in range(N)]
+        values = centers[i].tolist()
+        values += values[:1]
+        angles += angles[:1]
+        # Initialise the spider plot
+        ax = plt.subplot(polar=True)
+        # Draw one axe per variable + add labels labels yet
+        plt.xticks(angles[:-1], cdf.columns, color='grey', size=8) 
+        # Plot data
+        ax.plot(angles, values, linewidth=1, linestyle='solid')
+        # Fill area
+        ax.fill(angles, values, 'b', alpha=0.1)
+    
+    plt.show()
+    """
+
+    plt.figure(figsize=(8, 4))
+    for i in range(0, len(centers)):
+        plt.plot(centers[i], marker='o', label='Cluster %s' % i)
+    plt.tick_params(axis='both', which='major', labelsize=22)
+    plt.xticks(range(0, len(cdf.columns)), cdf.columns, fontsize=18)
+    plt.legend(fontsize=20)
+    plt.show()
     """
     sn.heatmap(cdf.drop('CustomerID', axis=1).corr(), annot=True)
     plt.show()
